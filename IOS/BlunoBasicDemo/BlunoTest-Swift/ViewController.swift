204a6d36c0766158575d8218c1e49df751d30221
Joe Longstreet <joelongstreet@gmail.com>
2016-06-07 08:51:52 -0500

    add a swift based target

---
+++ IOS/BlunoBasicDemo/BlunoTest-Swift/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BlunoTest-Swift
+//
+//  Created by Joe Longstreet on 6/7/16.
+//  Copyright © 2016 DFRobot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
