5c32ec94fcfee4b0a26d32d04cc0957765ba6fd8
Joe Longstreet <joelongstreet@gmail.com>
2016-06-09 00:45:16 -0500

    hook up labels, fields, and a button to complete a basic ui

--- IOS/BlunoBasicDemo/BlunoTest-Swift/ViewController.swift
+++ IOS/BlunoBasicDemo/BlunoTest-Swift/ViewController.swift
@@ -11,16 +11,21 @@ import UIKit
 class ViewController: UIViewController, DFBlunoDelegate, UITableViewDelegate, UITableViewDataSource {
     
     let blunoManager = DFBlunoManager.sharedInstance() as! DFBlunoManager
+    var blunoDevice = DFBlunoDevice()
     var discoveredDevices: [DFBlunoDevice] = []
     
     @IBOutlet var tableView: UITableView!
-    
+    @IBOutlet var dataReceivedLabel: UILabel!
+    @IBOutlet var dataReceivedField: UITextField!
+    @IBOutlet var dataToSendField: UITextField!
+    @IBOutlet var dataToSendLabel: UILabel!
+    @IBOutlet var sendButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.blunoManager.delegate = self;
-        self.blunoManager.scan();
+        self.blunoManager.delegate = self
+        self.blunoManager.scan()
         
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
@@ -38,7 +43,7 @@ class ViewController: UIViewController, DFBlunoDelegate, UITableViewDelegate, UI
     }
     
     func didDiscoverDevice(dev: DFBlunoDevice!) {
-        var deviceFound = false;
+        var deviceFound = false
         
         for device in self.discoveredDevices {
             if device.isEqual(dev) {
@@ -47,30 +52,38 @@ class ViewController: UIViewController, DFBlunoDelegate, UITableViewDelegate, UI
         }
         
         if(!deviceFound){
-            self.discoveredDevices.append(dev);
+            self.discoveredDevices.append(dev)
             self.tableView.reloadData()
         }
     }
     
     func readyToCommunicate(dev: DFBlunoDevice!) {
-        
+        self.blunoDevice = dev;
     }
     
     func didDisconnectDevice(dev: DFBlunoDevice!) {
-        
+        NSLog("Disconnected \(dev.name)")
     }
     
     func didWriteData(dev: DFBlunoDevice!) {
-        
+        NSLog("Writing data to \(dev.name)")
     }
     
     func didReceiveData(data: NSData!, device dev: DFBlunoDevice!) {
-        
+        let textString = NSString.init(data: data, encoding: NSUTF8StringEncoding)
+        dataReceivedField.text = textString as? String
     }
     
+    // MARK: UI Event Handlers
+    @IBAction func buttonClick(sender: AnyObject) {
+        let textString = dataToSendField.text
+        let data = textString?.dataUsingEncoding(NSUTF8StringEncoding)
+        self.blunoManager.writeDataToDevice(data, device: self.blunoDevice)
+    }
+
     // MARK: TableViewDelegate and TableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.discoveredDevices.count;
+        return self.discoveredDevices.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -84,11 +97,27 @@ class ViewController: UIViewController, DFBlunoDelegate, UITableViewDelegate, UI
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        NSLog(self.discoveredDevices[indexPath.row].name)
+        self.blunoDevice = self.discoveredDevices[indexPath.row]
+        self.blunoManager.connectToDevice(self.blunoDevice)
+        
+        dataReceivedField.enabled = true
+        dataToSendField.enabled = true
+        sendButton.enabled = true
+        
+        dataReceivedLabel.textColor = UIColor.blackColor()
+        dataToSendLabel.textColor = UIColor.blackColor()
     }
     
     func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-        NSLog(self.discoveredDevices[indexPath.row].name)
+        self.blunoDevice = self.discoveredDevices[indexPath.row]
+        self.blunoManager.disconnectToDevice(self.blunoDevice)
+        
+        dataReceivedField.enabled = false
+        dataToSendField.enabled = false
+        sendButton.enabled = false
+        
+        dataReceivedLabel.textColor = UIColor.grayColor()
+        dataToSendLabel.textColor = UIColor.grayColor()
     }
 }
 
