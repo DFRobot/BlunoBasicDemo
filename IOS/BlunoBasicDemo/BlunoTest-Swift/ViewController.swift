4808f4b19b156e00e48b54be44e42dc0b0b7960e
Joe Longstreet <joelongstreet@gmail.com>
2016-06-08 23:16:14 -0500

    create a ui table view and list bluno devices

--- IOS/BlunoBasicDemo/BlunoTest-Swift/ViewController.swift
+++ IOS/BlunoBasicDemo/BlunoTest-Swift/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, DFBlunoDelegate {
+class ViewController: UIViewController, DFBlunoDelegate, UITableViewDelegate, UITableViewDataSource {
     
     let blunoManager = DFBlunoManager.sharedInstance() as! DFBlunoManager
     var discoveredDevices: [DFBlunoDevice] = []
+    
+    @IBOutlet var tableView: UITableView!
     
     
     override func viewDidLoad() {
@@ -19,6 +21,8 @@ class ViewController: UIViewController, DFBlunoDelegate {
         
         self.blunoManager.delegate = self;
         self.blunoManager.scan();
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
     
     override func didReceiveMemoryWarning() {
@@ -44,6 +48,7 @@ class ViewController: UIViewController, DFBlunoDelegate {
         
         if(!deviceFound){
             self.discoveredDevices.append(dev);
+            self.tableView.reloadData()
         }
     }
     
@@ -61,6 +66,29 @@ class ViewController: UIViewController, DFBlunoDelegate {
     
     func didReceiveData(data: NSData!, device dev: DFBlunoDevice!) {
         
+    }
+    
+    // MARK: TableViewDelegate and TableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.discoveredDevices.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let blunoDevice = self.discoveredDevices[indexPath.row]
+        let cellLabel = "\(blunoDevice.name): \(blunoDevice.identifier)"
+        
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        cell.textLabel?.text = cellLabel
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        NSLog(self.discoveredDevices[indexPath.row].name)
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        NSLog(self.discoveredDevices[indexPath.row].name)
     }
 }
 
